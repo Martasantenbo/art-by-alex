682cc59d338490b6f980d493bf989a5bfba7a1ea
Martasantenbo <martasan.donuts@gmail.com>
2022-07-06 21:51:28 +0200

    add(GalleryMain.vue) added format

--- src/components/GalleryMain.vue
+++ src/components/GalleryMain.vue
@@ -3,9 +3,15 @@ import Modal from './Modal.vue';
 </script>
 <template>
   <article>
+    <div class="bg-primary-pastel py-2 md:(flex gap-18 justify-center) xl:(flex gap-18 justify-center)">
+      <div class="text-20px leading-normal md:text-24px xl:text-24px text-primary">Format:</div>
+      <div class="text-20px leading-normal md:text-24px xl:text-24px text-primary">30:30</div>
+      <div class="text-20px leading-normal md:text-24px xl:text-24px text-primary">40:40</div>
+      <div class="text-20px leading-normal md:text-24px xl:text-24px text-primary">140:140</div>
+    </div>
     <div class="up">
       <div class="down">
-        <h2 class="text-white text-36px py-7 md:(text-secondary text-44px) xl:(text-primary text-56px py-12)">Galerie
+        <h2 class="text-white text-36px py-7 md:(text-secondary text-48px) xl:(text-primary text-56px py-12)">Galerie
         </h2>
         <div class="flex flex-wrap gap-19 justify-center">
           <div v-for="n in 9" class="bg-green-500 min-w-80 min-h-80 xl:(min-w-90 min-h-90)">{{ n }}</div>

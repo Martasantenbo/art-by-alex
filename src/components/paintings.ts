5c4ef494da79ae8a7b96113a2fe139021f3b2e71
Martasantenbo <martasan.donuts@gmail.com>
2022-07-11 17:03:24 +0200

    add(paintings.ts) added component

---
+++ src/components/paintings.ts
@@ -0,0 +1,112 @@
+export const paintings = [
+  {
+    header: '220401',
+    image: '../assets/paintings/220401.jpg',
+    video: '../assets/paintings/220401.mov'
+  },
+  {
+    header: '220402',
+    image: '../assets/paintings/220402.jpg',
+    video: '../assets/paintings/220402.mov'
+  },
+  {
+    header: '220403',
+    image: '../assets/paintings/220403.jpg',
+    video: '../assets/paintings/220403.mov'
+  },
+  {
+    header: '220404',
+    image: '../assets/paintings/220404.jpg',
+    video: '../assets/paintings/220404.mov'
+  },
+  {
+    header: '22040501',
+    image: '../assets/paintings/22040501.jpg',
+    video: '../assets/paintings/22040501.mov'
+  },
+  {
+    header: '22040502',
+    image: '../assets/paintings/22040502.jpg',
+    video: '../assets/paintings/22040502.mov'
+  },
+  {
+    header: '220406',
+    image: '../assets/paintings/220406.jpg',
+    video: '../assets/paintings/220406.mov'
+  },
+  {
+    header: '220501',
+    image: '../assets/paintings/220501.jpg',
+    video: '../assets/paintings/220501.mov'
+  },
+  {
+    header: '220502',
+    image: '../assets/paintings/220502.jpg',
+    video: '../assets/paintings/220502.mov'
+  },
+  {
+    header: '220503',
+    image: '../assets/paintings/220503.jpg',
+    video: '../assets/paintings/220503.mov'
+  },
+  {
+    header: '220504',
+    image: '../assets/paintings/220504.jpg',
+    video: '../assets/paintings/220504.mov'
+  },
+  {
+    header: '220505',
+    image: '../assets/paintings/220505.jpg',
+    video: '../assets/paintings/220505.mov'
+  },
+  {
+    header: '220506',
+    image: '../assets/paintings/220506.jpg',
+    video: '../assets/paintings/220506.mov'
+  },
+  {
+    header: '220507',
+    image: '../assets/paintings/220507.jpg',
+    video: '../assets/paintings/220507.mov'
+  },
+  {
+    header: '220508',
+    image: '../assets/paintings/220508.jpg',
+    video: '../assets/paintings/220508.mov'
+  },
+  {
+    header: '220601',
+    image: '../assets/paintings/220601.jpg',
+    video: '../assets/paintings/220601.mov'
+  },
+  {
+    header: '220602',
+    image: '../assets/paintings/220602.jpg',
+    video: '../assets/paintings/220602.mov'
+  },
+  {
+    header: '220603',
+    image: '../assets/paintings/220603.jpg',
+    video: '../assets/paintings/220603.mov'
+  },
+  {
+    header: '220604',
+    image: '../assets/paintings/220604.jpg',
+    video: '../assets/paintings/220604.mov'
+  },
+  {
+    header: '220605',
+    image: '../assets/paintings/220605.jpg',
+    video: '../assets/paintings/220605.mov'
+  },
+  {
+    header: '220606',
+    image: '../assets/paintings/220606.jpg',
+    video: '../assets/paintings/220606.mov'
+  },
+  {
+    header: '220607',
+    image: '../assets/paintings/220607.jpg',
+    video: '../assets/paintings/220607.mov'
+  },
+]
